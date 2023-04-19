5a8567731c01f6ee60750afb84212b4db6a06656
Ashwin Padmanabhan <ashwin.ap1094@gmail.com>
2023-04-19 14:30:07 -0400

    js added

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var topics = ["HTML", "CSS", "JS", "Git"];
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x])
+}
+
+// if (topic === "HTML") {
+//     console.log("Lets study HTML");
+// }
+// else if (topic === "CSS") {
+//     console.log("Lets study CSS");
+// }
+// else if (topic === "Git") {
+//     console.log("Lets study Git");
+// }
+// else if (topic === "JS") {
+//     console.log("Lets study JS");
+// }
+// else {
+//     console.log("Try again later");
+// }
+
+
